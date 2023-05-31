5f2eff3c917558a6222057c3477a31932c647317
Tiendino <87251869+Tiendino@users.noreply.github.com>
2023-05-31 21:37:05 +0700

    Add files via upload
    hello

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em iu';
+const giftUrl = '';
+const eventName = 'Chúc mừng sinh nhật';
+const titleCard = 'Gửi bạn Huế';
+const contentCard = '❤️️Em yêu của anh ngày hôm nay là sinh nhật em, anh mong rằng năm nào anh cũng cùng đón sinh nhật em với tư cách là người yêu em, anh chúc mọi điều tốt lành sẽ đến với em, mong em luôn cảm thấy hạnh phúc khi ở bên cạnh anh,anh rất thích nhìn em cười, em như 1 phần không thể thiếu của anh vậy. Anh yêu em rất nhiều❤️️ ';
+const giftImage = 'em.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
